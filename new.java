e0c22cb6270be6012ee28ba48a0cf3080490c014
nilanjonaarpita <nilanjonaarpita@gmail.com>
2025-10-25 23:21:56 +0600

    Add files via upload

---
+++ new.java
@@ -0,0 +1,14 @@
+ import java.util.*
+ public class Condition{
+    public static void main(String args[]){
+        scanner sc= new scanner(System.in);
+        int age=sc.next Int();
+         
+        if(age>18){
+            System.Out.print("Adult");
+        }else{
+            System.Out.print("Not Adult");
+        }
+    }
+ }
+
